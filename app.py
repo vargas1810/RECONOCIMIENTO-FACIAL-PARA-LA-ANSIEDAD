7037e30df0fd129b20c7806fe494552488673ddc
vargas1810 <jeffer.vargas.1810@gmail.com>
2024-10-10 22:30:52 -0500

    COMMMIT RECONOCIMIENTO VARGAS QUISPE JEFFERSON GRUPO 1

--- app.py
+++ app.py
@@ -126,4 +126,6 @@ def download_file(filename):
 
 if __name__ == '__main__':
     os.makedirs('uploads', exist_ok=True)
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))  # Usa el puerto de la variable de entorno o 5000 como predeterminado
+    app.run(debug=True, host='0.0.0.0', port=port)
+
